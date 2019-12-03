685fa5fbf48a84f01750d080dfdac030e8661a86
a.bazylewicz <arkadiusz.bazylewicz@adform.com>
2019-12-03 17:27:02 +0100

    Ex2 part 2

--- src/bin/ex2.rs
+++ src/bin/ex2.rs
@@ -1,21 +1,31 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("ex2.input").expect("Something went wrong reading the file");
-    let mut array: Vec<usize> = contents
-        .split(",")
-        .map(|v| v.parse().expect("wrong number"))
-        .collect();
-    let mut run = true;
-    let mut offset: usize = 0;
-    array[1] = 12;
-    array[2] = 2;
-    while run {
-        run = compute(&mut array, offset);
-        offset = offset + 4;
-    }
+    for noun in 1..99 {
+        for verb in 1..99 {
+            let contents =
+                fs::read_to_string("ex2.input").expect("Something went wrong reading the file");
 
-    println!("{:?}", array);
+            let mut array: Vec<usize> = contents
+                .split(",")
+                .map(|v| v.parse().expect("wrong number"))
+                .collect();
+            let mut run = true;
+            let mut offset: usize = 0;
+            array[1] = noun;
+            array[2] = verb;
+            while run {
+                run = compute(&mut array, offset);
+                offset = offset + 4;
+            }
+            if array[0] == 19690720 {
+                println!("{} {}", noun, verb);
+                return;
+            }
+        }
+    }
+    println!("Not found!");
+    return;
 }
 
 fn compute(input: &mut Vec<usize>, offset: usize) -> bool {
@@ -26,16 +36,15 @@ fn compute(input: &mut Vec<usize>, offset: usize) -> bool {
             let arg2_idx = input[offset + 2];
             let result_idx = input[offset + 3];
             input[result_idx] = input[arg1_idx] + input[arg2_idx];
-        },
+        }
         2 => {
             let arg1_idx = input[offset + 1];
             let arg2_idx = input[offset + 2];
             let result_idx = input[offset + 3];
             input[result_idx] = input[arg1_idx] * input[arg2_idx];
-        },
+        }
         99 => return false,
         _ => return false,
     }
     return true;
 }
-
